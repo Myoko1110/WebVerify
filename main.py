96b33e10b38c21d7e6b39d9aec87f2505c64c80d
Myoko <92731952+MyokoJP@users.noreply.github.com>
2023-04-29 16:03:02 +0900

    Fix: いろいろなバグ修正

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import asyncio
 import secrets
 import threading
+from datetime import datetime
 
 import discord
 from discord.ext.commands import Cog, Bot
 from flask import Flask, render_template, request
+import pytz
 import yaml
 
 
@@ -13,7 +16,6 @@ with open('settings.yml', 'r') as f:
 token = settings['Token']
 RoleID = settings['RoleID']
 Domain = settings['Domain']
-role_id = settings['role_id']
 site_key = settings['site_key']
 secret_key = settings['secret_key']
 
@@ -21,17 +23,21 @@ client = discord.Client(intents=discord.Intents.all())
 tree = discord.app_commands.CommandTree(client)
 app = Flask(__name__)
 
+# sessionの辞書宣言
 session = {}
 
 
+# ロールを追加
 async def add_role(user, guild):
     guild = client.get_guild(guild)
     member = guild.get_member(user)
-    role = discord.utils.get(guild.roles, id=role_id)
+    role = guild.get_role(RoleID)
     if role:
         await member.add_roles(role)
+        await member.send("認証が完了しました!")
 
 
+# ボタンの処理
 class VerifyView(discord.ui.View):
     def __init__(self, timeout=180):
         super().__init__(timeout=timeout)
@@ -40,14 +46,14 @@ class VerifyView(discord.ui.View):
     @discord.ui.button(label="認証", style=discord.ButtonStyle.success)
     async def verify(self, inter: discord.Interaction, button: discord.ui.Button):
         global session
-
         # 変数を作成
         random = secrets.token_urlsafe(32)
         await inter.response.send_message(f"こちらのURLから認証を完了させてください：http://{Domain}?id={random}", ephemeral=True)
 
         # セッションを変数に追加
+        now = datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
         user = inter.user.id
-        lists = [user, inter.guild.id]
+        lists = {"user": user, "server": inter.guild.id, "time": now}
         session[random] = lists
         print(session)
 
@@ -58,6 +64,7 @@ async def on_ready():
     print('ready')
 
 
+# コマンドの処理
 @tree.command(name='verify', description='認証メッセージを送信します')
 async def verify(ctx: discord.Interaction):
     if ctx.user.guild_permissions.administrator:
@@ -71,28 +78,33 @@ async def verify(ctx: discord.Interaction):
         await ctx.response.send_message('認証メッセージを送信しました。', ephemeral=True)
 
 
+# webの処理
 @app.route('/', methods=["GET", "POST"])
-async def not_robot():
-    global session
-    print(session)
-    if request.method == "GET":
+def not_robot():
         id = request.args.get("id")
-        if id is None or id == "":
-            return render_template('index.html', id=False)
+        if id is None or id == "" or not id in session:
+            return render_template('index.html', id=False, msg="IDが不明です")
         else:
-            return render_template('index.html', id=True, site_key=site_key, secret_key=secret_key)
-    else:
-        id = request.args.get("id")
+            return render_template('index.html', id=True, site_key=site_key, secret_key=secret_key, session=id)
 
-        session_list = session[id]
-        user = session_list[0]
-        guild = session_list[1]
-        guild = client.get_guild(guild)
-        member = guild.get_member(user)
-        role = discord.utils.get(guild.roles, id=role_id)
-        if role:
-            await member.add_roles(role)
-        return render_template('index.html', id='success')
+
+# 認証成功の処理
+@app.route('/complete', methods=["GET", "POST"])
+def complete():
+    # print(session)
+    id = request.args.get("id")
+
+    if id is None or id == "" or not id in session:
+        return render_template('index.html', id=False, msg="IDが不明です")
+
+    session_list = session[id]
+    user = session_list["user"]
+    guild = session_list["server"]
+    asyncio.run_coroutine_threadsafe(add_role(user, guild), client.loop)
+
+    del session[id]
+
+    return render_template('index.html', id=False, msg="認証が完了しました")
 
 
 # flaskとdiscord.pyを起動
